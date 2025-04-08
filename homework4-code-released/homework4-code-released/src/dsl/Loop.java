ae82a3531e04cf019f776c58b17a3e29046e41c9
Stefan <sb121@rice.edu>
2025-04-08 17:50:08 -0500

    passing 2 loop test cases

--- homework4-code-released/homework4-code-released/src/dsl/Loop.java
+++ homework4-code-released/homework4-code-released/src/dsl/Loop.java
@@ -6,25 +6,49 @@ import utils.Or;
 
 public class Loop<A,B> implements Query<A,B> {
 
-	// TODO
+    Query<Or<A,B>,B> q;
+    boolean first;
+    B last;
+    SLastCount<B> sink1;
+    long prevCount;
 
 	public Loop(Query<Or<A,B>,B> q) {
-		// TODO
+		this.q = q;
+        this.first = true;
+        this.sink1 = new SLastCount<B>();
+        this.prevCount = 0;
 	}
 
 	@Override
 	public void start(Sink<B> sink) {
-		// TODO
-	}
+        q.start(sink1);
+        if (sink1.count != prevCount) {
+            prevCount = sink1.count;
+            sink.next(sink1.last);
+        }
+    }
 
 	@Override
 	public void next(A item, Sink<B> sink) {
-		// TODO
+        if (first) {
+            q.next(Or.inl(item), sink1);
+            if (sink1.last != null) {
+                first = false;
+                sink.next(sink1.last);
+            }
+        } else  {
+            q.next(Or.inr(((SLastCount<B>)sink1).last), sink1);
+            if (((SLastCount<B>)sink1).count != prevCount) {
+                prevCount = ((SLastCount<B>)sink1).count;
+                sink.next(sink1.last);
+            }
+
+        }
 	}
 
 	@Override
 	public void end(Sink<B> sink) {
-		// TODO
+		sink.end();
 	}
 	
 }
--- homework4-code-released/homework4-code-released/src/dsl/Pipeline.java
+++ homework4-code-released/homework4-code-released/src/dsl/Pipeline.java
@@ -7,6 +7,7 @@ public class Pipeline<A,B,C> implements Query<A,C> {
 	Query<A,B> q1;
     Query<B,C> q2;
     SLastCount<B> sink1;
+    long count;
 
 
 	public Pipeline(Query<A,B> q1, Query<B,C> q2) {
@@ -14,17 +15,22 @@ public class Pipeline<A,B,C> implements Query<A,C> {
         this.q2 = q2;
         this.sink1 = new SLastCount<B>();
         q1.start(sink1);
+        this.count = 0;
 	}
 
 	@Override
 	public void start(Sink<C> sink) {
-		// Do nothing
+		q1.start(sink1);
+        q2.start(sink);
 	}
 
 	@Override
 	public void next(A item, Sink<C> sink) {
 		q1.next(item, sink1);
-        q2.next(sink1.last, sink);
+        if (sink1.count != count) {
+            count = sink1.count;
+            q2.next(sink1.last, sink);
+        }
 	}
 
 	@Override
