599e1deeba7392f5733d4491d7780baf95a75c39
Stefan <sb121@rice.edu>
2025-04-08 22:27:56 -0500

    done

--- homework4-code-released/homework4-code-released/src/dsl/Loop.java
+++ homework4-code-released/homework4-code-released/src/dsl/Loop.java
@@ -7,14 +7,12 @@ import utils.Or;
 public class Loop<A,B> implements Query<A,B> {
 
     Query<Or<A,B>,B> q;
-    boolean first;
     B last;
     SLastCount<B> sink1;
     long prevCount;
 
 	public Loop(Query<Or<A,B>,B> q) {
 		this.q = q;
-        this.first = true;
         this.sink1 = new SLastCount<B>();
         this.prevCount = 0;
 	}
@@ -24,25 +22,18 @@ public class Loop<A,B> implements Query<A,B> {
         q.start(sink1);
         if (sink1.count != prevCount) {
             prevCount = sink1.count;
+            q.next(Or.inr(sink1.last), sink1);
             sink.next(sink1.last);
         }
     }
 
 	@Override
 	public void next(A item, Sink<B> sink) {
-        if (first) {
-            q.next(Or.inl(item), sink1);
-            if (sink1.last != null) {
-                first = false;
-                sink.next(sink1.last);
-            }
-        } else  {
-            q.next(Or.inr(((SLastCount<B>)sink1).last), sink1);
-            if (((SLastCount<B>)sink1).count != prevCount) {
-                prevCount = ((SLastCount<B>)sink1).count;
-                sink.next(sink1.last);
-            }
-
+        q.next(Or.inl(item), sink1);
+        if (sink1.count != prevCount) {
+            prevCount = sink1.count;
+            sink.next(sink1.last);
+            q.next(Or.inr(sink1.last), sink);
         }
 	}
 
